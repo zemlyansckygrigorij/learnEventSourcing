77d6f1ca60405df306b291b6b77060ca0ce032d7
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2020-10-20 17:54:04 +0500

    create module axonTrader

---
+++ eventSourcing/src/test/java/ru/trademrg/queue/EventQueueImplTest.java
@@ -0,0 +1,39 @@
+package ru.trademrg.queue;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import ru.trademrg.events.CreatedTradeEvent;
+
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EventQueueImplTest {
+
+    @Test
+    void queueTest() {
+        //given
+        var events = List.of(CreatedTradeEvent.builder().tradeId(1).build(),
+                CreatedTradeEvent.builder().tradeId(2).build(),
+                CreatedTradeEvent.builder().tradeId(3).build(),
+                CreatedTradeEvent.builder().tradeId(4).build(),
+                CreatedTradeEvent.builder().tradeId(5).build());
+
+        //when
+        var storage = new EventQueueImpl();
+        for (var event : events) {
+            storage.putEvent(event);
+        }
+        //then
+        assertThat(storage.size()).isEqualTo(events.size());
+
+        //when
+        for (CreatedTradeEvent event : events) {
+            assertThat(storage.getEvent()).isEqualTo(event);
+        }
+        //then
+        assertThat(storage.size()).isZero();
+    }
+}
